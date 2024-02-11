cb7402375b23dc978057f0758ec28298bfcb155d
Prajna-5432 <prajnamarakal@gmail.com>
2024-02-11 19:02:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const https=require('https');
+const fs= require('fs');
+
+const app = express();
+// const PORT = 3000;
+
+const urlMap = {};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.post('/shorten', (req, res) => {
+  const originalUrl = req.body.originalUrl;
+  const customShortcode = req.body.customShortcode; // Add this line to get custom shortcode
+
+  if (customShortcode && urlMap[customShortcode]) {
+    return res.status(400).json({ error: 'Custom shortcode is already in use.' });
+  }
+
+  const shortUrl = customShortcode || generateShortUrl();
+  urlMap[shortUrl] = originalUrl;
+  res.json({ shortUrl: `http://localhost:${PORT}/${shortUrl}` });
+});
+
+// app.get('/:id', (req, res)=> {
+//     console.log(req.params);
+//     res.send('hi')
+// })
+app.get('/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const originalUrl = urlMap[shortUrl];
+  if (originalUrl) {
+    res.redirect(301, originalUrl); // Use a permanent redirect (301)
+  } else {
+    res.status(404).send('Not Found');
+  }
+});
+
+function generateShortUrl() {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let shortUrl = '';
+
+  for (let i = 0; i < 7; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortUrl += characters.charAt(randomIndex);
+  }
+
+  return shortUrl;
+}
+
+// Wrap the Express app with serverless
+module.exports.handler = serverless(app);
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+//   });
